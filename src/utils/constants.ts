8be7fe79253601ffaa0f513241510f74fe2f2dd9
Tjaz <tjaz@gizmo.moe>
2022-09-21 16:18:30 +0200

    don't use bright green for success log type

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { Color } from "@utils/enums";
 
 export const LOG_COLORS = {
 	INFO: Color.Cyan,
-	SUCCESS: Color.BrightGreen,
+	SUCCESS: Color.Green,
 	WARNING: Color.Yellow,
 	ERROR: Color.Red
 };
